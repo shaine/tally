31db8f5bcaa340a9c357fe65851bd15f33d79cb2
Shaine Hatch <shaine@squidtree.com>
2024-10-14 23:31:52 -0600

    Add type to  metric

--- lib/tally/tracker/metric.ex
+++ lib/tally/tracker/metric.ex
@@ -2,9 +2,12 @@ defmodule Tally.Tracker.Metric do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @types boolean: "The metric occurred for a day, or not", count: "Events are counted per day"
+
   schema "metrics" do
     field :name, :string
     field :description, :string
+    field :type, Ecto.Enum, values: Keyword.keys(@types)
 
     timestamps(type: :utc_datetime)
   end
@@ -12,7 +15,9 @@ defmodule Tally.Tracker.Metric do
   @doc false
   def changeset(metric, attrs) do
     metric
-    |> cast(attrs, [:name, :description])
-    |> validate_required([:name, :description])
+    |> cast(attrs, [:name, :description, :type])
+    |> validate_required([:name, :description, :type])
   end
+
+  def types, do: @types
 end
